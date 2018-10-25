6300a1979cb8b1108116ddb245f9b3c7cafd3d94
noah <noahwoodward2244@gmail.com>
2018-10-25 11:38:14 -0700

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Noah Woodward on 10/25/18.
+//  Copyright © 2018 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
